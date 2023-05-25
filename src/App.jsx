287fbc8c80e8063075d05a4d8cad9733026f2a8e
Arjun S <arjun8107@gmail.com>
2023-05-25 22:32:17 +0530

    Migrated to react-pewpew and made some html chages

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,18 @@ import { StreamLanguage } from "@codemirror/language";
 import { lua } from "@codemirror/legacy-modes/mode/lua";
 import { dracula } from "thememirror";
 import {PewPewString} from "react-pewpew"
+import { useHotkeys } from 'react-hotkeys-hook'
 
 function App() {
   const [code, setCode] = useState("pewpew.print('hello world')");
-  const [buffer, setBuffer] = useState("");
+  const [buffer, setBuffer] = useState(code);
   const onChange = useCallback((value, viewUpdate) => {
     setBuffer(value);
   }, []);
+  useHotkeys('ctrl+s', () => setCode(buffer),{
+    preventDefault:true,
+    enableOnContentEditable:true
+  }, [buffer])
   return (
     <div className="App">
       <div className="code" id="codemirror-editor">
@@ -47,7 +52,7 @@ function App() {
             d="M19.376 12.416L8.777 19.482A.5.5 0 0 1 8 19.066V4.934a.5.5 0 0 1 .777-.416l10.599 7.066a.5.5 0 0 1 0 .832z"
           />
         </svg>
-        Run
+        Run (CTRL+S)
       </button>
     </div>
   );
